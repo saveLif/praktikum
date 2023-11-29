f8302425a1bafe2a5e95c8af585fa728cb5ab0b7
saveLif <nanazizou80@gmail.com>
2023-11-29 23:26:08 +0100

    add icon-tree to card

--- Frontend/baum/src/app/components/map/map.component.ts
+++ Frontend/baum/src/app/components/map/map.component.ts
@@ -4,6 +4,80 @@ import { MarkerService } from '../../shared/marker.service';
 import { GpsService } from '../../shared/gps.service';
 import 'leaflet.markercluster';
 
+// const iconRetinaUrl = 'assets/marker-icon-2x.png';
+// const iconUrl = 'assets/marker-icon.png';
+// const shadowUrl = 'assets/marker-shadow.png';
+// const iconDefault = L.icon({
+//   iconRetinaUrl,
+//   iconUrl,
+//   shadowUrl,
+//   iconSize: [25, 41],
+//   iconAnchor: [12, 41],
+//   popupAnchor: [1, -34],
+//   tooltipAnchor: [16, -28],
+//   shadowSize: [41, 41],
+// });
+// L.Marker.prototype.options.icon = iconDefault;
+
+// @Component({
+//   selector: 'app-map',
+//   templateUrl: './map.component.html',
+//   styleUrls: ['./map.component.scss'],
+// })
+// export class MapComponent implements OnInit {
+//   private map!: L.Map;
+//   private markers!: L.MarkerClusterGroup;
+
+//   constructor(
+//     private markerService: MarkerService,
+//     private userGPS: GpsService
+//   ) {}
+
+//   ngOnInit(): void {
+//     this.initMap();
+//   }
+
+//   private initMap(): void {
+//     this.map = L.map('map', {
+//       center: [52.2646577, 10.5236066],
+//       zoom: 12,
+//     });
+
+//     const OSM = L.tileLayer(
+//       'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+//       {
+//         maxZoom: 19,
+//         minZoom: 3,
+//         attribution:
+//           '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+//       }
+//     );
+
+//     var CartoDB_Voyager = L.tileLayer(
+//       'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+//       {
+//         attribution:
+//           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+//         subdomains: 'abcd',
+//         maxZoom: 19,
+//         minZoom: 3,
+//       }
+//     );
+
+//     CartoDB_Voyager.addTo(this.map);
+
+//     this.markers = L.markerClusterGroup(); // Crée un groupe de regroupement de marqueurs
+
+//     this.map.addLayer(this.markers); // Ajoute le groupe de regroupement de marqueurs à la carte
+
+//     this.markerService.makeMarker(this.markers, 'object'); // Ajoute les marqueurs au groupe de regroupement
+
+//     this.userGPS.getUserLocation(this.map);
+//   }
+// }
+
+// ... (previous imports)
+
 const iconRetinaUrl = 'assets/marker-icon-2x.png';
 const iconUrl = 'assets/marker-icon.png';
 const shadowUrl = 'assets/marker-shadow.png';
@@ -66,11 +140,23 @@ export class MapComponent implements OnInit {
 
     CartoDB_Voyager.addTo(this.map);
 
-    this.markers = L.markerClusterGroup(); // Crée un groupe de regroupement de marqueurs
 
-    this.map.addLayer(this.markers); // Ajoute le groupe de regroupement de marqueurs à la carte
+    this.markers = L.markerClusterGroup({
+      iconCreateFunction: (cluster) => {
+        const count = cluster.getChildCount();
+        const treeIconUrl = 'assets/arbre-icon.png';
 
-    this.markerService.makeMarker(this.markers, 'object'); // Ajoute les marqueurs au groupe de regroupement
+        return L.divIcon({
+          html: `<div><span>${count}</span><img src="${treeIconUrl}" /></div>`,
+          className: 'custom-cluster-icon',
+          iconSize: [20, 20]
+        });
+      },
+    });
+
+    this.map.addLayer(this.markers);
+
+    this.markerService.makeMarker(this.markers, 'object');
 
     this.userGPS.getUserLocation(this.map);
   }
