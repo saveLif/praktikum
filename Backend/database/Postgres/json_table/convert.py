2a04279298e7dcc8dd37ede03762c38e87e596bf
saveLif <nanazizou80@gmail.com>
2024-01-04 16:49:22 +0100

    convert.py angepasst

--- Backend/database/Postgres/json_table/convert.py
+++ Backend/database/Postgres/json_table/convert.py
@@ -1,18 +1,16 @@
 import json
 import psycopg2
 
-# importiert JSON-Datenbank von Vivien Gräf in Postgres
-
+# importiert JSON-Datenbank von Vivien Gräf in Postgres und dann von Nana Batta bearbeitet
 
 # Stellt Verbindung zur Postgres Datenbank her
-
 try:
     conn = psycopg2.connect(
-	database="Wiese",
-	user="Bruno",
-	password="Rum",
-	host="localhost",
-	port="5432"
+    database='gruenflaeche',
+    user='julian1234',
+    password='baum',
+    host='localhost',
+    port='5432'
     )
 except psycopg2.Error as e:
 
@@ -20,35 +18,46 @@ except psycopg2.Error as e:
 
 cur = conn.cursor()
 
+file_paths = [
+    "C:\\telegraf\\db.JSON",
+    "C:\\telegraf\\description.JSON",
+    "C:\\telegraf\\measured_values.JSON",
+    "C:\\telegraf\\object.JSON",
+    "C:\\telegraf\\sensor.JSON",
+    "C:\\telegraf\\sensor_node.JSON",
+]
+
+object_data = {}
+
 try:
-
-
-    with open("/home/codetree/baum-backend/database/Tablefiles/json/db.json", "r") as f:
-        object_data = json.load(f)
+    for file_path in file_paths:
+        with open(file_path, "r") as f:
+            data = json.load(f)
+            object_data[file_path] = data
 except FileNotFoundError:
      print("Die angegebene JSON-Datei wurde nicht gefunden.")
 
 
 
-sql_create_table_geolocation = "CREATE TABLE IF NOT EXISTS geolocation(ID serial PRIMARY KEY, latitude numeric NOT NULL, longitude numeric NOT NULL)"
-sql_create_table_object = """CREATE TABLE IF NOT EXISTS object(
+sql_create_table_geolocation = "CREATE TABLE geolocation(ID serial PRIMARY KEY, latitude numeric NOT NULL, longitude numeric NOT NULL)"
+sql_create_table_object = """CREATE TABLE object(
     ID serial PRIMARY KEY,
     description text NOT NULL,
     geolocation_id integer NOT NULL REFERENCES geolocation(id),
     description_ID integer NOT NULL REFERENCES description(ID)
 )"""
-sql_create_table_located =  "CREATE TABLE IF NOT EXISTS located(object_id integer NOT NULL REFERENCES object(ID), sensor_id integer NOT NULL REFERENCES sensor(ID))"
+sql_create_table_located =  "CREATE TABLE located(object_id integer NOT NULL REFERENCES object(ID), sensor_id integer NOT NULL REFERENCES sensor(ID))"
 try:
     cur.execute(sql_create_table_geolocation)
-    cur.execute("CREATE TABLE IF NOT EXISTS name(ID serial PRIMARY KEY, german text NOT NULL, botanical text NOT NULL)")
-    cur.execute("CREATE TABLE IF NOT EXISTS description(ID serial PRIMARY KEY, name_id integer NOT NULL REFERENCES name(id), origin text NOT NULL, leaf_shape text NOT NULL, general_description text NOT NULL)")
-    cur.execute("CREATE TABLE IF NOT EXISTS flowering(month smallint NOT NULL, description_id integer NOT NULL REFERENCES description(ID))")
-    cur.execute("CREATE TABLE IF NOT EXISTS location(ID serial PRIMARY KEY, geolocation_id integer NOT NULL REFERENCES geolocation(id), height integer NOT NULL)")
-    cur.execute("CREATE TABLE IF NOT EXISTS sensor(ID serial PRIMARY KEY, measured_variable text NOT NULL, installation_date date NOT NULL, location_id integer NOT NULL REFERENCES location(id))")
+    cur.execute("CREATE TABLE name(ID serial PRIMARY KEY, german text NOT NULL, botanical text NOT NULL)")
+    cur.execute("CREATE TABLE description(ID serial PRIMARY KEY, name_id integer NOT NULL REFERENCES name(id), origin text NOT NULL, leaf_shape text NOT NULL, general_description text NOT NULL)")
+    cur.execute("CREATE TABLE flowering(month smallint NOT NULL, description_id integer NOT NULL REFERENCES description(ID))")
+    cur.execute("CREATE TABLE location(ID serial PRIMARY KEY, geolocation_id integer NOT NULL REFERENCES geolocation(id), height integer NOT NULL)")
+    cur.execute("CREATE TABLE sensor(ID serial PRIMARY KEY, measured_variable text NOT NULL, installation_date date NOT NULL, location_id integer NOT NULL REFERENCES location(id))")
     
     cur.execute(sql_create_table_object)
     cur.execute(sql_create_table_located)
-    cur.execute("CREATE TABLE IF NOT EXISTS measured_values(ID serial PRIMARY KEY, value integer NOT NULL, timestamp timestamp NOT NULL, sensor_ID integer NOT NULL REFERENCES sensor(id))")    
+    cur.execute("CREATE TABLE measured_values(ID serial PRIMARY KEY, value integer NOT NULL, timestamp timestamp NOT NULL, sensor_ID integer NOT NULL REFERENCES sensor(id))")    
 except psycopg2.Error as e:
     print(e)
 description_ids = {}    
